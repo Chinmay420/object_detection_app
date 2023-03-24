54733a74a8d46433dda0fce69d624bd91006ea0d
Chinmay <chinmaychamoli5@gmail.com>
2023-03-23 23:03:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def process_image(img):
 def main():
     st.set_page_config(page_title="My Streamlit App", page_icon=":memo:", layout="wide", initial_sidebar_state="expanded")
     st.title("Road object predictor")
-    st.markdown('<style>div{color: Maroon;}</style>', unsafe_allow_html=True)
-    st.markdown('<style>h1{color: Maroon;}</style>', unsafe_allow_html=True)
+    st.markdown('<style>div{color: #003300;}</style>', unsafe_allow_html=True)
+    st.markdown('<style>h1{color: #003300;}</style>', unsafe_allow_html=True)
     img = st.file_uploader('Insert your image', type = ['png'])
     img =process_image(img)
     result = ""
